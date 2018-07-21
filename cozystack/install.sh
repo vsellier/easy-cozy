513f505ede418fe405a5af764ab617d610331196
Vincent Sellier <vsellier@exoplatform.com>
2018-07-21 23:23:45 +0200

    download checksum from github

--- cozystack/install.sh
+++ cozystack/install.sh
@@ -7,22 +7,22 @@ NODE_ARCH=""
 if [[ $ARCH =~ "x86" ]]; then
 	COZY_ARCH=amd64
 	NODE_ARCH=x64
-	SHA256="f86c88a99981ed9c64289465d68a97f991af743834981691074688d5d039e6f3"
 elif [[ $ARCH =~ "arm" ]]; then
 	COZY_ARCH=arm
 	NODE_ARCH=armv7l
-	SHA256="1c3cc78362471effc7a9fd7f52accac943ab0a009e4d31f4c4d850dffd4d444b"
 else
 	echo "Unknown arch $ARCH"
 	exit 1
 fi
 
 echo "Install cozy-stack..."
-cat <<EOF >/tmp/SHA256
-SHA256(/tmp/cozy)= ${SHA256}
-EOF
 
 wget -O /tmp/cozy https://github.com/cozy/cozy-stack/releases/download/${COZY_VERSION}/cozy-stack-linux-${COZY_ARCH}
+wget -O /tmp/cozy.sha256 https://github.com/cozy/cozy-stack/releases/download/${COZY_VERSION}/cozy-stack-linux-${COZY_ARCH}.sha256
+
+cat <<EOF >/tmp/SHA256
+SHA256(/tmp/cozy)= $(awk '{print $2}' cozy.sha256)
+EOF
 
 echo "Expected checkum                : $(cat /tmp/SHA256)"
 echo "Checksum of the downloaded file : $(sha256sum --tag /tmp/cozy)"
