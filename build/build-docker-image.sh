4960960f3377dba3a8b55216c23a88c6afd02b7b
Vincent Sellier <vsellier@exoplatform.com>
2018-07-27 21:07:41 +0200

    support version suffix on docker image

--- build/build-docker-image.sh
+++ build/build-docker-image.sh
@@ -12,14 +12,18 @@ PUSH=false
 function usage() {
 	echo "$0 [-v <version>] [-p] [-h]"
 	echo "-v <version>  : Specify the version to build default: latest"
+	echo "-s <suffix>   : Specify a suffix to add to the version name <version>_<suffix>"
 	echo "-p            : Push the image to docker hub"
 	echo "-h            : display this help"
 }
 
-while getopts "v:ph" opt; do
+while getopts "v:phs:" opt; do
 	case $opt in
 	v)
 		VERSION=${OPTARG}
+		;;
+	s)
+		VERSION_SUFFIX=${OPTARG}
 		;;
 	p)
 		PUSH=true
@@ -39,8 +43,13 @@ if [[ "${LOCAL_ARCH}" =~ "arm" ]]; then
 fi
 
 COMPLETE_IMAGE_NAME="${IMAGE}:${VERSION}-${IMAGE_ARCH}"
+if [ -n  "${VERSION_SUFFIX:-}" ]; then
+	COMPLETE_IMAGE_NAME="${COMPLETE_IMAGE_NAME}_${VERSION_SUFFIX}"
+fi
 
 pushd "${SCRIPT_DIR}/../cozystack" || exit
+
+echo "Preparing easy-cozy image version ${COMPLETE_IMAGE_NAME}"
 
 echo "Refreshing base image..."
 docker pull debian:stable-slim
