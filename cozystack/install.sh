6b5044f3152c322dac8fd792f222f3359952e7e6
Vincent SELLIER <vincent.sellier@gmail.com>
2020-12-29 16:18:44 +0100

    formatting

--- cozystack/install.sh
+++ cozystack/install.sh
@@ -20,21 +20,20 @@ fi
 
 echo "Install cozy-stack..."
 # for an obscure reason, in arm/v7, wget didn't recognize the digicert certificate if the ca-certificate path is not specified
-wget --ca-certificate=/etc/ssl/certs/ca-certificates.crt -O /tmp/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION} https://github.com/cozy/cozy-stack/releases/download/${COZY_VERSION}/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION}
-wget --ca-certificate=/etc/ssl/certs/ca-certificates.crt -O /tmp/cozy.sha256 https://github.com/cozy/cozy-stack/releases/download/${COZY_VERSION}/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION}.sha256
+wget --ca-certificate=/etc/ssl/certs/ca-certificates.crt -O "/tmp/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION}" "https://github.com/cozy/cozy-stack/releases/download/${COZY_VERSION}/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION}"
+wget --ca-certificate=/etc/ssl/certs/ca-certificates.crt -O /tmp/cozy.sha256 "https://github.com/cozy/cozy-stack/releases/download/${COZY_VERSION}/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION}.sha256"
 
-awk "{print \"SHA256(/tmp/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION})=\",\$2}" /tmp/cozy.sha256 > /tmp/SHA256
-
+awk "{print \"SHA256(/tmp/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION})=\",\$2}" /tmp/cozy.sha256 >/tmp/SHA256
 
 echo "Expected checkum                : $(cat /tmp/SHA256)"
 echo "Checksum of the downloaded file : $(sha256sum --tag /tmp/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION})"
 sha256sum -c /tmp/SHA256
 
-mv /tmp/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION} /tmp/cozy
+mv "/tmp/cozy-stack-linux-${COZY_ARCH}-${COZY_VERSION}" /tmp/cozy
 chmod u+x /tmp/cozy
 
 echo "Install nodejs..."
 
-wget --ca-certificate=/etc/ssl/certs/ca-certificates.crt -O /tmp/node.tar.xz https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-${NODE_ARCH}.tar.xz
+wget --ca-certificate=/etc/ssl/certs/ca-certificates.crt -O /tmp/node.tar.xz "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-${NODE_ARCH}.tar.xz"
 tar -xv --use-compress-program xz -f node.tar.xz
-mv node-v${NODE_VERSION}-linux-${NODE_ARCH} node
+mv "node-v${NODE_VERSION}-linux-${NODE_ARCH}" node
